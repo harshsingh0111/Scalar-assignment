4ccfed0f9f727a069b1c529538b803edd4eebdd9
harshsingh0111 <164759042+harshsingh0111@users.noreply.github.com>
2026-03-28 15:30:30 +0530

    fixed db crash

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,21 +1,15 @@
 const mysql = require("mysql2/promise");
 
-const connection = await mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    waitForConnections: true,
-    connectionLimit: 10,
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error("DB connection failed:", err);
-    } else {
-        console.log("Connected to Railway MySQL 🚀");
-    }
-});
+console.log("Connected to Railway MySQL 🚀");
 
 module.exports = pool;
