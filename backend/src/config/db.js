bf45cde67208a4c0d8fe8a88be89ce834b081284
harshsingh0111 <164759042+harshsingh0111@users.noreply.github.com>
2026-03-28 13:56:02 +0530

    connected backend to railways

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root@123",
-    database: "trello_clone",
+const connection = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
-module.exports = pool.promise();
+connection.connect((err) => {
+    if (err) {
+        console.error("DB connection failed:", err);
+    } else {
+        console.log("Connected to Railway MySQL 🚀");
+    }
+});
+
+module.exports = connection;
